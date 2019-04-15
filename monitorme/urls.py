2837d2010e0a3a4c0cad8a39c6f816ec34709189
Craig Brunengraber <brunengraber@hotmail.com>
2019-04-15 09:46:59 -0400

    fixed postgresql and some bugs

--- monitorme/urls.py
+++ monitorme/urls.py
@@ -1,4 +1,4 @@
-"""portfolio URL Configuration
+"""monitorme URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
