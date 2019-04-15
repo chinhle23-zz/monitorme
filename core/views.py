309ec3446575b5e4bf0a1bcf9f391af39b7b1170
aaronsellek <aaron.sellek1@gmail.com>
2019-04-15 15:20:27 -0400

    fixed view for all(userprofile), added userprofile url

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from core.models import User, TrackerGroup, Question, Answer, Response
 # Create your views here.
 
 def user_profile(request, username):
-    user = User.objects.get(username=username)
+    user = User.objects.all()
     return render(request, 'user_profile.html', {"user":user})
