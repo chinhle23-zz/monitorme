44874ae0bb4bf2bb5de33e6c260087b899667bf7
aaronsellek <aaron.sellek1@gmail.com>
2019-04-15 14:03:11 -0400

    template for userprofile, and url for index page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from core import views as core_views
 
 urlpatterns = [
-
+    path('', core_views.index, name='index'),
 ]
